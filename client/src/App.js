d87df9d20b22949150fa167ff0892c13323fa8d5
aledia <alediaortega@gmail.com>
2019-05-09 09:57:02 +0200

    changes

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import { Switch, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+     
+    </div>
+  );
+}
+
+export default App;
